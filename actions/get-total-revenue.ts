6274af901e58a1015a839ddda303fae45a430c3d
thibaud-g <thibaud.gippet@gmail.com>
2023-09-29 10:23:16 +0200

    ts ignore

--- actions/get-total-revenue.ts
+++ actions/get-total-revenue.ts
@@ -1,4 +1,6 @@
+// @ts-ignore
 import prismadb from "@/lib/prismadb";
+import { Order, OrderItem } from "@prisma/client";
 
 export const getTotalRevenue = async (storeId: string) => {
   const paidOrders = await prismadb.order.findMany({
@@ -15,7 +17,7 @@ export const getTotalRevenue = async (storeId: string) => {
     }
   });
 
-  const totalRevenue = paidOrders.reduce((total : number, order) => {
+  const totalRevenue = paidOrders.reduce((total, order) => {
     const orderTotal = order.orderItems.reduce((orderSum, item) => {
       return orderSum + item.product.price.toNumber();
     }, 0);
